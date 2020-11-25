75fe12bdf3ff79717ef505a67b19fc74144ec670
Rezan92 <rezanibrahim8@gmail.com>
2020-11-25 12:32:39 +0100

    Added more details to icon's alt

--- week1/homework/react-exercises/src/Guarantee.js
+++ week1/homework/react-exercises/src/Guarantee.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./assets/style/Guarantee.css";
 
 const Guarantee = ({ services }) => {
-  const { img, title, description } = services;
-
+  const { img, title, description , icon} = services;
+console.log(services);
   return (
     <div className="services-content">
-      <img src={img} alt="icon" />
+      <img src={img} alt={`${icon} icon`} />
       <h1>{title}</h1>
       <p>{description}</p>
     </div>
--- week1/homework/react-exercises/src/services.js
+++ week1/homework/react-exercises/src/services.js
@@ -1,28 +1,31 @@
-import chat from "./assets/images/chat.png";
-import coin from "./assets/images/coin.png";
-import f_delivery from "./assets/images/f-delivery.png";
+import Chat from "./assets/images/chat.png";
+import Coin from "./assets/images/coin.png";
+import F_delivery from "./assets/images/f-delivery.png";
 
 const services = [
     {
-      img: f_delivery,
+      img: F_delivery,
       title: "Free shipping",
       description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
         Vel quidem earum et quis quos, modi ex ipsa quaerat eum recusandae sunt,
          iste sint libero.`,
+         icon: "Delivery"
     },
     {
-      img: coin,
+      img: Coin,
       title: "100% Money back",
       description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
       Vel quidem earum et quis quos, modi ex ipsa quaerat eum recusandae sunt,
        iste sint libero.`,
+       icon: "Coin"
     },
     {
-      img: chat,
+      img: Chat,
       title: "Online support 24/7",
       description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
       Vel quidem earum et quis quos, modi ex ipsa quaerat eum recusandae sunt,
        iste sint libero.`,
+       icon: "Chat"
     },
   ];
 
